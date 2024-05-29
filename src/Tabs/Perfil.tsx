94f0b00ddfe99275a425705793223204791df0cf
Yaacer <yandabombasenai@gmail.com>
2024-05-29 16:31:11 -0300

    Botão de busca funcionando
    Agora o botão de busca puxa o que foi pesquisado

--- src/Tabs/Perfil.tsx
+++ src/Tabs/Perfil.tsx
@@ -39,6 +39,7 @@ export default function Perfil({navigation}) {
         <Titulo color="blue.500">Informações pessoais</Titulo>
         <Titulo fontSize="lg" mb={1}>{dadosPaciente.nome}</Titulo>
         <Text>{dadosPaciente.email}</Text>
+        <Text>{dadosPaciente?.endereco?.estado}</Text>
 
         <Divider mt={5} />
 
